b3d21d1e79eb8d04acde4cc33a474264527dfd22
Shakthi-Viviyn <55011804+Shakthi-Viviyn@users.noreply.github.com>
2023-07-26 17:05:32 -0400

    20230726/app-completed

--- src/App.js
+++ src/App.js
@@ -1,119 +1,146 @@
 import React, { useState, useEffect} from 'react';
 import './App.css';
 import { languageData } from './languageData';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
 
-  const [jokeData, setJokeData] = useState({
+  const [rawJokeData, setRawJokeData] = useState({
     setup: '',
     delivery: ''
   });
 
+  const [jokeData, setJokeData] = useState({
+    setup: '',
+    delivery: ''
+  })
+
   const [languagesList, setLanguagesList] = useState([]);
   const [selectedLanguage, setSelectedLanguage] = useState('en');
 
-
-  useEffect(() => {
-    fetchJoke();
-  }, []);
-
+  
+  //async function fetches joke from API
   const fetchJoke = async () => {
     try {
       const response = await fetch('https://v2.jokeapi.dev/joke/Any?safe-mode&type=twopart'); 
 
       if (!response.ok) {
-        throw new Error('Network response was not ok.');
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
       const jsonData = await response.json();
-      setJokeData({
+      setRawJokeData({
         setup: jsonData.setup,
         delivery: jsonData.delivery
       });
 
     } catch (error) {
+      setRawJokeData({
+        setup: '-',
+        delivery: '-'
+      })
+      toast.error('Unable to fetch joke');
       console.error('Error fetching data:', error);
     }
   };
 
-  function handleNewJoke() {
-    fetchJoke();
-  }
-
-  useEffect(() => {
+  //async function fetches supported languages from Google API
+  const fetchLanguages = async () => {
     const apiKey = process.env.REACT_APP_API_KEY;
     const url = `https://translation.googleapis.com/language/translate/v2/languages?key=${apiKey}`;
-
-    const fetchLanguages = async () => {
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        if (data && data.data && data.data.languages) {
-          setLanguagesList(data.data.languages);
-        } else {
-          console.error('Error fetching supported languages: Invalid API response');
-        }
-      } catch (error) {
-        console.error('Error fetching supported languages:', error.message);
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
-    };
 
-    fetchLanguages();
-  }, []);
-
-  useEffect(() => {
-    async function translateText(text, targetLanguage) {
-      const apiKey = process.env.REACT_APP_API_KEY;
-      const url = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`;
-      console.log("translated text")
-      const requestBody = {
-        q: text,
-        target: targetLanguage,
-      };
-    
-      const requestOptions = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(requestBody),
-      };
-    
-      try {
-        const response = await fetch(url, requestOptions);
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-    
-        const data = await response.json();
-        if (data && data.data && data.data.translations) {
-          const translation = data.data.translations[0].translatedText;
-          return translation;
-        } else {
-          console.error('Error fetching translation: Invalid API response');
-        }
-      } catch (error) {
-        console.error('Error fetching translation:', error.message);
+      const data = await response.json();
+      if (data && data.data && data.data.languages) {
+        setLanguagesList(data.data.languages);
+      } else {
+        toast.error('Translation unavailable');
+        console.error('Error fetching supported languages: Invalid API response');
       }
+    } catch (error) {
+      toast.error('Translation unavailable');
+      console.error('Error fetching supported languages:', error.message);
     }
+  };
+  
+  // async function translates text from english to selected language
+  async function translateText(text, targetLanguage) {
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const url = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`;
+    const requestBody = {
+      q: text,
+      target: targetLanguage,
+    };
+  
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestBody),
+    };
+  
+    try {
+      const response = await fetch(url, requestOptions);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      if (data && data.data && data.data.translations) {
+        const translation = data.data.translations[0].translatedText;
+        return translation;
+      } else {
+        toast.error('Translation failed');
+        console.error('Error fetching translation: Invalid API response');
+      }
+    } catch (error) {
+      toast.error('Translation failed');
+      console.error('Error fetching translation:', error.message);
+    }
+    return text;
+  }
 
-    let translatedDelivery = '';
-    let translatedSetup = '';
-    (async () => {
-      translatedSetup = await translateText(jokeData.setup, selectedLanguage);
-      translatedDelivery = await translateText(jokeData.delivery, selectedLanguage);
+  //async function translates the two parts of the joke using translateText function
+  const translateJoke = async () => {
+    if (selectedLanguage !== 'en') {
+      let translatedDelivery = '';
+      let translatedSetup = '';
+      translatedSetup = await translateText(rawJokeData.setup, selectedLanguage);
+      translatedDelivery = await translateText(rawJokeData.delivery, selectedLanguage);
 
       setJokeData({
         setup: translatedSetup,
         delivery: translatedDelivery
       })
-    })();
-    
-  }, [selectedLanguage]); 
+    }else{
+      setJokeData({
+        setup: rawJokeData.setup,
+        delivery: rawJokeData.delivery
+      })
+    }
+  }
+
+  //hook fetches the first joke and list of languages
+  useEffect(() => {
+    fetchJoke();
+    fetchLanguages();
+  },[]);
+
+  //hook translates the joke every time selected language changes or new joke is fetched
+  useEffect(() => {
+    translateJoke();
+  }, [selectedLanguage, rawJokeData]);
+
+  //button press event handlers
+  function handleNewJoke(){
+    fetchJoke();
+  }
 
   function updateLanguage(e) {
     setSelectedLanguage(e.target.value);
@@ -121,13 +148,11 @@ function App() {
 
   return (
     <div className="App">
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light"/>
+
       <div className="main-container">
-        <h1>{jokeData.setup}</h1>
-        <h4>{jokeData.delivery}</h4>
-
-        <button onClick={handleNewJoke}>New Joke</button>
-
-        <select value={selectedLanguage} onChange={updateLanguage}>
+        <div className='language-selector-container'>
+        <select value={selectedLanguage} onChange={updateLanguage} className='language-selector'>
           {languagesList.map((currentLanguage) => (
             languageData[currentLanguage.language] &&
             <option key={currentLanguage.language} value={currentLanguage.language}>
@@ -135,7 +160,13 @@ function App() {
             </option>
           ))}
         </select>
-        
+        </div>
+
+        <div className='content-box'>
+          <h1>{jokeData.setup}</h1>
+          <h2>{jokeData.delivery}</h2>
+          <button id="new-joke-btn" onClick={handleNewJoke}>New Joke</button>
+        </div>
       </div>
     </div>
   );
